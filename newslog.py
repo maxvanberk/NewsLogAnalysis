d5634ac654be2908181e9dfab1caea604e350793
maxvanberk <vanberk@outlook.com>
2017-06-05 18:22:04 +0200

    Better Output

--- newslog.py
+++ newslog.py
@@ -7,10 +7,12 @@ def get_article_leaderboard():
     """What are the most popular three articles of all time? """
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
+    columns_descr = c.description
     c.execute("select * from article_leaderboard")
     result_articles = c.fetchall()
     for row in result_articles:
-        print row[0]
+        x, y = row
+        print('Article: ' + '%s\t' % (x)+ 'Views: ' + '%s' % (y))
     db.close()
 
 
@@ -18,10 +20,12 @@ def get_author_leaderboard():
     """Who are the most popular article authors of all time?"""
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
+    columns_descr = c.description
     c.execute("select * from author_leaderboard;")
     result_authors = c.fetchall()
     for row in result_authors:
-        print row[0]
+        x, y = row
+        print('Author: ' + '%s\t' % (x)+ 'Views: ' + '%s' % (y))
     db.close()
 
 
@@ -29,6 +33,7 @@ def get_bad_days():
     """On which days did more than 1% of requests lead to errors?"""
     db = psycopg2.connect("dbname=news")
     c = db.cursor()
+    columns_descr = c.description
     c.execute("select * from bad_days;")
     result_days = c.fetchall()
     for row in result_days:
